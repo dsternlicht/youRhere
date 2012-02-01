b233b5c26eb4bcb37d9a936b598d73ac8a681e44
fastrd <gdblogst@gmail.com>
2012-02-01 18:36:49 +0200

    v1.3 - added firstTimeWizard  and bug fixes

---
+++ README.md
@@ -0,0 +1,105 @@
+# youRhere jQuery plugin
+
+* http://yourhere.gandtblog.com/
+* Copyright 2012, Daniel Sternlicht
+* http://www.danielsternlicht.com
+* Dual licensed under the MIT or GPL Version 2 licenses.		
+* Enjoy :)
+
+## What does "youRhere" jQuery plugin do?
+
+"youRhere" is an awesome jQuery plugin which give your readers a chance to mark their article reading- progress by just clicking it.
+
+## Features
+
+* 100% Free
+* Using HTML5 local storage API
+* Full "rtl" & "ltr" support
+* First time usage wizard
+* Very easy to implement
+* Cross Browsers support
+* Multiple configurability options
+* 1 JS file - Only 7k
+
+## Usage
+
+As I said, it's very easy to implement "youRhere" in your website. All you have to do is to include the "jquery.urhere.js" file, and then activate it on the relevant container. For example:
+
+	<script type="text/javascript" src="js/jQuery.Last.Version"></script> 
+	<script type="text/javascript" src="js/jquery.yourhere.js"></script> 
+	<script type="text/javascript">	
+	$(document).ready(function(){	
+		$('#content').yourhere();	
+	});	
+	</script>
+
+## Options
+
+When you activate the plugin, the activation function can pass a few arguments which allow you to control the plugin. Here is a list of options with their default settings:
+
+	firstTimeWizard: true,
+	useLocalStorage: true,
+	autoScrollToMarker: true, 
+	markerDirection: 'left',
+	tempMarkerBackground: '#B7B7B7',
+	markerLineBackground: '#FFF82A',
+	markerBackgroundOpacity: '0.7',
+	markerBackground: '#000',
+	markerWidth: '5px', 
+	supportedElements: 'h1, h2, h3, h4, h5, h6, p, ul, li'
+	
+Let's have a quick look about "youRhere" options.
+
+### firstTimeWizard
+
+Sets the activation of the "first time wizard" when the plugin detect it's the first time the user enter this website. Accept "true" or "false".
+
+### useLocalStorage
+
+Controls the HTML5 Local Storage API that allow the user to save his reading progress by marking a line. Accept "true" or "false".
+
+### autoScrollToMarker
+
+In case your "useLocalStorage" is set to "true" you can specified if you want the users window to scroll automatically to the last line they have marked. Accept "true" or "false".
+
+### markerDirection
+
+Controls which way the marker will appear. Your choices between 'right' or 'left'.
+
+### tempMarkerBackground
+
+Controls the scrolling "marker" background. Sets a "Background" property to the ".yourhere-tempMarker" DIV. Basically can accept anything you want including background colors, images, positions and so on..
+
+### markerLineBackground
+
+Controls the background color of the highlighted line. Accept any color with any format you want. f.e "black", "#000000", "rgb(0,0,0)".
+
+### markerBackgroundOpacity
+
+Sets an "Opacity" property to the highlighted line background. Accept any number in the range of 0.0 - 1.0.
+
+### markerBackground
+
+Controls the "marker" element background. Sets a "Background" property to the ".yourhere-marker" DIV. Can accept anything you want including background colors, images, positions and so on..
+
+### markerWidth
+
+Sets a "Width" property to the Marker element ('.yourhere-marker'). Accept a number of pixels. f.e "20px"
+
+### supportedElements
+
+Sets which DOM elements will respond to the youRhere click event (highlighted text). Accept multiple elements, should be seperated by ",". f.e: "p, h2, blockquote" - will trigger the click event on paragraphs, heading 2 text and blockquotes.
+
+## Demo
+
+You can see a few examples on our Demo Page at: http://yourhere.gandtblog.com/demo.html
+
+## Release
+
+* Current 1.3 beta - Added "first time wizard", changed functionality and fixed some bugs. Last updated on 2/2/12. 
+* 1.2 beta - Added scroll to last marker option and bug fixes. Last updated on 31/1/12. 
+* 1.1 beta - Added HTML5 Local Storage support and bug fixes. Last updated on 30/1/12. 
+* 1.0 beta - "youRhere" jQuery plugin. Last updated on 27/1/12.
+
+Don't forget to like & share youRhere from:
+http://yourhere.gandtblog.com/
--- jquery.yourhere.js
+++ jquery.yourhere.js
@@ -1,5 +1,5 @@
 /**************************************************************
-	* youRhere v1.2 - jQuery Plugin
+	* youRhere v1.3 - jQuery Plugin
 	* http://yourhere.gandtblog.com/
 	* Copyright 2012, Daniel Sternlicht
 	* http://www.danielsternlicht.com
@@ -25,6 +25,7 @@
 			// Set a relative position to the container childrens
 			box.children().css({zIndex: basicZindex + 2, position: 'relative'});
 			
+			// Create the marker
 			$('<div class="yourhere-marker"></div>').hide().appendTo(box).css({
 				position: 'absolute',
 				zIndex: basicZindex + 3,
@@ -34,6 +35,7 @@
 				height: boxLineHeight + 'px'		
 			});
 			
+			// Create the marker highlight line
 			$('<div class="yourhere-markerline"></div>').hide().appendTo(box).css({
 				position: 'absolute',
 				zIndex: basicZindex + 1,
@@ -48,27 +50,28 @@
 			// Create the temporary marker
 			$.yourhere.tempMarker(box, boxLineHeight, basicZindex);						
 			
-			// Events Listener
-			$.yourhere.eventsHandeler(box, boxLineHeight);
+			// Events handler
+			$.yourhere.eventsHandler(box, boxLineHeight);
 						
 		},
 		
-		eventsHandeler: function(box, boxLineHeight) {
+		eventsHandler: function(box, boxLineHeight) {
 			var boxOffset = box.offset().top;
 			
 			// Click event on an element
 			box.children($.yourhere.opts.supportedElements).click(function(e){
-				var lineHeight = parseFloat($(this).css('lineHeight')) > parseFloat($(this).css('fontSize')) ? parseFloat($(this).css('lineHeight')) : parseFloat($(this).css('fontSize'));	
-				var elmOffset = $(this).offset().top;
-				var z = e.pageY - elmOffset;
-				var e = Math.floor(z / lineHeight);
-				var y = elmOffset - boxOffset + (e * lineHeight);
-				$.yourhere.markerCreator(box, y, lineHeight);
-				if(localStorage && $.yourhere.opts.useLocalStorage){
-					localStorage.removeItem('yourhere_' + window.location.href);
-					localStorage.setItem('yourhere_' + window.location.href, y + "," + lineHeight);
+				if(localStorage && !localStorage.getItem('yourhere_first_time') && $.yourhere.opts.firstTimeWizard) {
+					placeMarker($(this), e);					
+					$.yourhere.firstTime(box);
+					box.children($.yourhere.opts.supportedElements).unbind('click');
 				}
-			});	
+			});
+			
+			// Double click event on an element
+			box.children($.yourhere.opts.supportedElements).dblclick(function(e){
+				placeMarker($(this), e);
+				if(localStorage && !localStorage.getItem('yourhere_first_time') && $.yourhere.opts.firstTimeWizard) $.yourhere.firstTime(box);
+			});
 
 			// Mouse move event
 			box.children().mousemove(function(e){
@@ -86,8 +89,21 @@
 				$(this).fadeOut('fast');
 				box.find('.yourhere-markerline').fadeOut('fast');
 				if(localStorage && $.yourhere.opts.useLocalStorage)
-					localStorage.removeItem('yourhere_' + window.location.href);
+					localStorage.removeItem('yourhere_' + window.location.pathname);
 			});
+			
+			function placeMarker(elm, e) {
+				var lineHeight = parseFloat(elm.css('lineHeight')) > parseFloat(elm.css('fontSize')) ? parseFloat(elm.css('lineHeight')) : parseFloat(elm.css('fontSize'));	
+				var elmOffset = elm.offset().top;
+				var z = e.pageY - elmOffset;
+				var e = Math.floor(z / lineHeight);
+				var y = elmOffset - boxOffset + (e * lineHeight);
+				$.yourhere.markerCreator(box, y, lineHeight);
+				if(localStorage && $.yourhere.opts.useLocalStorage){
+					localStorage.removeItem('yourhere_' + window.location.pathname);
+					localStorage.setItem('yourhere_' + window.location.pathname, y + "," + lineHeight);
+				}
+			}
 		},
 		
 		tempMarker: function(box, lineHeight, z) {
@@ -128,7 +144,7 @@
 		
 		checkStorage: function(box) {
 			if(localStorage && $.yourhere.opts.useLocalStorage) {
-				var data = localStorage.getItem('yourhere_' + window.location.href);
+				var data = localStorage.getItem('yourhere_' + window.location.pathname);
 				if(data && data != '') {
 					var arr = data.split(',');
 					$.yourhere.markerCreator(box, parseInt(arr[0]), parseInt(arr[1]));
@@ -141,22 +157,139 @@
 					}										
 				}
 			} else {
-				if(localStorage) localStorage.removeItem('yourhere_' + window.location.href);
+				if(localStorage) localStorage.removeItem('yourhere_' + window.location.pathname);
 				return;
 			}
 		},
+		
+		firstTime: function(box) {
+			// Check if tooltip already exist
+			if(box.find('.yourhere-tooltip').length <= 0) {
+				
+				// Create the tooltip
+				$('<div class="yourhere-tooltip"><div class="yourhere-tooltip-step"></div></div>').appendTo(box).hide()
+				.css({
+					width: '220px',
+					height: '100px',
+					padding: '10px',
+					background: '#eee',
+					position: 'absolute',
+					top: '0',
+					border: '1px solid #c7c7c7',
+					borderRadius: '5px',
+					boxShadow: '0 1px 2px rgba(0,0,0,0.4), inset 0 0 1px #fff',
+					lineHeight: '18px',
+					fontSize: '12px',
+					zIndex: '999'
+				});
+				
+				var tooltip = box.find('.yourhere-tooltip');				
+				tooltip.css($.yourhere.opts.markerDirection, -(parseFloat(tooltip.outerWidth()) / 2) + 'px')
+				.find('.yourhere-tooltip-step').html($.yourhere.firstTimeSteps[0]);
+				
+				$('<div class="yourhere-tooltip-cornerT"></div>').appendTo(tooltip)
+				.css({
+					width: 0,
+					height: 0,
+					borderTop: '9px solid #eee',
+					borderRight: '10px solid transparent',
+					borderLeft: '10px solid transparent',
+					position: 'absolute',
+					bottom: '-9px',
+					left: parseFloat(tooltip.width()) / 2 - 5 + 'px',
+					color: '#909090',
+					zIndex: '999'
+				});
+				
+				$('<div class="yourhere-tooltip-cornerB"></div>').appendTo(tooltip)
+				.css({
+					width: 0,
+					height: 0,
+					borderTop: '10px solid #c7c7c7',
+					borderRight: '11px solid transparent',
+					borderLeft: '11px solid transparent',
+					position: 'absolute',
+					bottom: '-10px',
+					left: parseFloat(tooltip.find('.yourhere-tooltip-cornerT').css('left')) - 1 + 'px',
+					zIndex: '998'
+				});
+				
+				$('<span class="yourhere-tooltip-close">x</span>').appendTo(tooltip)
+				.css({
+					position: 'absolute',
+					top: '0',
+					right: '5px',
+					fontSize: '12px',
+					color: '#a7a7a7',
+					cursor: 'pointer',
+					zIndex: '1000'
+				});
+				
+				$('<a class="yourhere-tooltip-next" href="#">next</a>').appendTo(tooltip).css({
+					position: 'absolute',
+					right: '10px',
+					bottom: '5px'
+				});
+			}
+			
+			// Place the tooltip relative to the youRhere marker
+			setTimeout(function(){
+				$('.yourhere-tooltip').fadeIn(function(){
+					$('.yourhere-tooltip').animate({top: parseFloat(box.find('.yourhere-marker').css('top')) - parseFloat($('.yourhere-tooltip').outerHeight()) - 13});
+				});
+			}, 1000);
+			
+			$.yourhere.firstTimeTutorialEvents(box, tooltip);
+		},
+		
+		firstTimeTutorialEvents: function(box, tooltip) {
+			var step = 0;
+			
+			// Close the tooltip
+			tooltip.find('.yourhere-tooltip-close').live('click', function(e){
+				e.preventDefault();
+				tooltip.fadeOut('normal', function(){
+					$(this).remove();
+				});
+				if(localStorage) localStorage.setItem('yourhere_first_time', false);
+			});
+			
+			// Next button events
+			tooltip.find('.yourhere-tooltip-next').live('click', function(e){
+				e.preventDefault();
+				step++;
+				tooltip.find('.yourhere-tooltip-step').fadeOut('fast', function(){
+					$(this).html($.yourhere.firstTimeSteps[step]).fadeIn('slow');
+				});
+				if(step == $.yourhere.firstTimeSteps.length - 1) {
+					var nextBtn = $(this);
+					nextBtn.removeClass('yourhere-tooltip-next').text('close');					
+					setTimeout(function(){
+						nextBtn.addClass('yourhere-tooltip-close');
+					}, 500);
+				}
+			});
+		},
+
+		firstTimeSteps: [
+			'<strong>Hey you</strong>! <br />We have noticed that this is the first time your\'re using "<a href="http://yourhere.gandtblog.com" target="_blank">youRhere</a>" marker in this website, so we made just for you a short "how to?" tutorial.',
+			'<strong>youRhere</strong> allow you to mark a line on the page by just <strong>double clicking it</strong>, so you could save your reading progress and come back to it later. <br />Don\'t be shy, try it now!',
+			'If you accidently marked a line and you want to remove the highlight from the page your\'e reading, you may do so by just <strong>double click on the marker itself</strong>.',
+			'That\'s it! <br /> This is all you have to know about <strong>youRhere</strong>. For more information you can visit <a href="http://yourhere.gandtblog.com" target="_blank">youRhere official website</a>. <br />Enjoy!'
+		],
 		
 		opts: {}
 		
 	}
 	
-	// Creating new instance for each call
+	// Creating new instance for each youRhere call
 	$.fn.yourhere = function( options ) {     
 		var yourhere = new $.yourhere.init( options, this );        
     };
 	
 	// youRhere default properties
 	$.fn.yourhere.defaults = {
+		firstTimeWizard: true,
 		useLocalStorage: true,
 		autoScrollToMarker: true,
 		markerDirection: 'left',
